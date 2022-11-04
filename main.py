512ef52130412c04ca49508fee491e1863564841
AngusOConnell <oconan193@gmail.com>
2022-11-05 06:10:58 +1100

    Adds input to leaderboard functionality

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import simulation, classes
+import simulation, leaderboard
 
 
 def main():
     # team = [classes.Pet(name, attack, health, level, exp, pos, held), ...]
     # battle_simulation([classes.Pet("fish", 2, 2, 1, 0, 0, None)], [classes.Pet("mosquito", 2, 2, 1, 0, 0, None)])
 
+    # add_to_leaderboard(1, [classes.Pet("fish", 2, 2, 1, 0, 0, None), classes.Pet("mosquito", 2, 2, 1, 0, 1, None)]
 
 # Battle simulation initiation function
 def battle_simulation(team1, team2):
@@ -24,6 +25,20 @@ def battle_simulation(team1, team2):
     print("Team 2 Win %: ", team2_win)
     print("Draw %: ", draw)
 
+# Adds team to leaderboard and returns score
+def add_to_leaderboard(turn, team):
+    team_str = ""
+    team_str = team_str + str(turn) + " "
+    for pet in team:
+        team_str = team_str + str(pet.name) + " "
+        team_str = team_str + str(pet.attack) + " "
+        team_str = team_str + str(pet.health) + " "
+        team_str = team_str + str(pet.level) + " "
+        team_str = team_str + str(pet.exp) + " "
+        team_str = team_str + str(pet.pos) + " "
+        team_str = team_str + str(pet.held) + " "
+
+    return leaderboard.add_to_leaderboard(team_str)
 
 if __name__ == "__main__":
     main()
