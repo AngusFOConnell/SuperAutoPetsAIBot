04b648f15ce90ee2748b595498be35619931e697
AngusOConnell <oconan193@gmail.com>
2022-11-06 03:28:31 +1100

    Generates all possible actions from given shop and team state

---
+++ actions.py
@@ -0,0 +1,47 @@
+import random
+import copy
+import classes
+
+
+# Generates all possible actions from given shop state
+def all_actions(shop, team):
+    # shop = [[pets], [food], gold]
+
+    actions = []
+
+    pets = shop[0]
+    foods = shop[1]
+    gold = shop[2]
+
+    if gold > 3:
+
+        # Buy Shop Pet onto team
+        if len(team) < 5:
+            for pet in pets:
+                actions.append(["buy_pet", pet, len(team)])
+
+        # level an existing pet on team from pet in shop
+        for shop_pet in pets:
+            for team_pet in team:
+                if shop_pet.name == team_pet.name and team_pet.level < 3:
+                    actions.append(["level", shop_pet, team_pet.pos])
+
+        # Buy food from shop
+        for food in foods:
+            for pet in team:
+                actions.append(["buy_food", food, pet.pos])
+
+    # Sell pet on team
+    for pet in team:
+        actions.append(["sell", pet, pet.pos])
+
+    # Roll shop
+    if gold > 1:
+        actions.append(["roll"])
+
+    # End turn
+    actions.append(["end_of_turn"])
+
+    return actions
+
+
